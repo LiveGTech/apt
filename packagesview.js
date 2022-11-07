0cb689615b7b869c1ff816e40121e22136ef93be
James-Livesey <jamesliveg@gmail.com>
2022-11-07 22:11:26 +0000

    Add working package search

--- packagesview.js
+++ packagesview.js
@@ -8,6 +8,7 @@
 */
 
 import * as astronaut from "https://opensource.liveg.tech/Adapt-UI/astronaut/astronaut.js";
+import Fuse from "./lib/fuse.esm.js";
 
 import * as packageData from "./packagedata.js";
 
@@ -20,17 +21,74 @@ $ sudo apt update\
 export var PackagesViewScreen = astronaut.component("PackagesViewScreen", function(props, children) {
     // TODO: Add package searching implementation
    
+    var searchInput = Input({type: "search", placeholder: _("searchForPackage_inputPlaceholder")}) ();
     var packageList = Container() ();
 
-    packageData.load().then(function(packages) {
+    var allPackages = [];
+    var displayedPackages = [];
+    var searcher = null;
+
+    function listDisplayedPackages() {
         packageList.clear().add(
-            ...packages.map((data) => Card (
+            ...displayedPackages.map((data) => Card (
                 Heading(1) (
                     CodeSnippet() (data.Package)
                 ),
                 Paragraph() (data.Description.length > 200 ? data.Description.substring(0, 200) + "…" : data.Description)
             ))
         );
+    }
+
+    searchInput.on("input", function() {
+        if (searchInput.getValue().trim() == "") {
+            displayedPackages = allPackages;
+
+            listDisplayedPackages();
+
+            return;
+        }
+
+        if (searcher == null) {
+            return;
+        }
+
+        var results = searcher.search(searchInput.getValue());
+
+        displayedPackages = results.map(function(result) {
+            return result.item;
+        });
+
+        if (displayedPackages.length > 0) {
+            listDisplayedPackages();
+        } else {
+            var searchDebianAptButton = Button() (_("searchForPackage_noResults_searchDebianApt"));
+
+            searchDebianAptButton.on("click", function() {
+                window.open(`https://packages.debian.org/search?keywords=${encodeURIComponent(searchInput.getValue())}`);
+            });
+
+            packageList.clear().add(
+                Message (
+                    Icon("search", "dark embedded") (),
+                    Heading(3) (_("searchForPackage_noResults_title")),
+                    Paragraph() (_("searchForPackage_noResults_description")),
+                    ButtonRow (
+                        searchDebianAptButton
+                    )
+                )
+            );
+        }
+    });
+
+    packageData.load().then(function(packages) {
+        searcher = new Fuse(packages, {
+            keys: ["Package", "Description"]
+        });
+
+        allPackages = packages;
+        displayedPackages = packages;
+
+        listDisplayedPackages();
     });
 
     return Screen(true) (
@@ -54,8 +112,7 @@ export var PackagesViewScreen = astronaut.component("PackagesViewScreen", functi
             ),
             Section (
                 Heading(2) (_("searchForPackage_title")),
-                // TODO: Implement searching to limit list
-                Input({type: "search", placeholder: _("searchForPackage_inputPlaceholder")}) (),
+                searchInput,
                 packageList
             )
         )
